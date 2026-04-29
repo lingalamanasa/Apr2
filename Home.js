96bd0a399356099c87df6db38ec7bd760815aa85
Lingala Manasa <153192851+lingalamanasa@users.noreply.github.com>
2026-04-29 15:36:16 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,11 @@
+//TASK-15
+function Home() {
+  return (
+    <div className="center">
+      <h1>Welcome to Foodie Restaurant 🍽️</h1>
+      <p>Delicious food with real images!</p>
+    </div>
+  );
+}
+
+export default Home;
