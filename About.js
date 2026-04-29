914921a3b1fe13b9af4b8928028968e366cfeed4
Lingala Manasa <153192851+lingalamanasa@users.noreply.github.com>
2026-04-29 15:35:13 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,11 @@
+//TASK-15
+function About() {
+  return (
+    <div className="center">
+      <h2>About Us</h2>
+      <p>We serve fresh and tasty food.</p>
+    </div>
+  );
+}
+
+export default About;
